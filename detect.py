9685b92b5755b48dcf191cb15fe80d9122b568a7
Meeeee6623 <c.benjaminsemail1@gmail.com>
2022-05-20 16:21:31 -0400

    add graphing, fix y bounds

--- detect.py
+++ detect.py
@@ -6,6 +6,12 @@ track_width = 2.0  # wheel center to center distance of car
 forward_constant = 1.0 # multiplier for speed of car, adjust for proper braking distance
 car_length = 6.0 # length of car from front to back wheels, center to center
 
+graph = False
+
+if graph:
+    import matplotlib.pyplot as plt
+    from matplotlib.patches import Rectangle
+
 
 def in_path(points, speed, angle):
 
@@ -17,17 +23,29 @@ def in_path(points, speed, angle):
 
     r_cf = np.hypot(r_center, car_length)  # front center radius
 
-    max_y = forward_constant * speed
+    y_max = forward_constant * speed
 
-    x_min = -track_width / \
-        2 if angle > 0 else np.sqrt(
-            np.pow(r_cf - track_width/2, 2) - np.pow(max_y, 2))
-    x_max = -track_width / \
+    x_min = r_center - track_width / \
         2 if angle < 0 else np.sqrt(
             np.pow(r_cf - track_width/2, 2) - np.pow(max_y, 2))
+    x_max = r_center + track_width / \
+        2 if angle > 0 else np.sqrt(
+            np.pow(r_cf + track_width/2, 2) - np.pow(max_y, 2))
 
     # filter points to x range
     points = points[points[0, :] <= x_max and points[0, :] >= x_min]
+
+    if graph:
+        fig, ax = plt.subplots()
+        ax.plot(0, 0, label='Turn Center')
+        ax.add_patch(Rectangle(r_center - track_width/2, 0), track_width, car_length)
+        x1 = np.linspace(r_center - track_width / 2, np.sqrt(np.pow(r_cf - track_width/2, 2) - np.pow(max_y, 2)), 100) # left boundary
+        x2 = np.linspace(r_center + track_width / 2, np.sqrt(np.pow(r_cf + track_width/2, 2) - np.pow(max_y, 2)), 100) # right boundary
+        y1 = np.sqrt(np.pow(r_center - track_width / 2, 2) - np.pow(x1, 2))
+        y2 = np.sqrt(np.pow(r_center + track_width / 2, 2) - np.pow(x2, 2))
+        ax.plot(x1, y1)
+        ax.plot(x2, y2)
+        plt.show()
 
     # check for points inside predicted car path
     return np.any(points[
@@ -36,6 +54,10 @@ def in_path(points, speed, angle):
         and
         points[1, :] <= np.sqrt(
             np.pow(points[0, :], 2) + np.pow(r_cf + track_width/2, 2))
+        and
+        points[1, :] >= car_length
+        and
+        points[1, :] <= y_max
     ])
 
 
