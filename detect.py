7648eccab4199ba10b94e8a65076061eb0042edb
Meeeee6623 <c.benjaminsemail1@gmail.com>
2022-05-20 17:03:51 -0400

    fix params

--- detect.py
+++ detect.py
@@ -3,8 +3,8 @@ import numpy as np
 
 
 track_width = 2.0  # wheel center to center distance of car
-forward_constant = 1.0 # multiplier for speed of car, adjust for proper braking distance
-car_length = 6.0 # length of car from front to back wheels, center to center
+forward_constant = 1.0  # multiplier for speed of car, adjust for proper braking distance
+car_length = 6.0  # length of car from front to back wheels, center to center
 
 graph = True
 
@@ -23,42 +23,46 @@ def in_path(points, speed, angle):
 
     r_cf = np.hypot(r_center, car_length)  # front center radius
 
-    y_max = forward_constant * speed
+    y_max = forward_constant * speed 
 
     x_min = r_center - track_width / \
         2 if angle < 0 else np.sqrt(
-            np.pow(r_cf - track_width/2, 2) - np.pow(max_y, 2))
+            np.power(r_cf - track_width/2, 2) - np.power(y_max, 2))
     x_max = r_center + track_width / \
         2 if angle > 0 else np.sqrt(
-            np.pow(r_cf + track_width/2, 2) - np.pow(max_y, 2))
+            np.power(r_cf + track_width/2, 2) - np.power(y_max, 2))
 
     # filter points to x range
-    points = points[points[:, 0] <= x_max and points[:, 0] >= x_min]
+    points = points[(points[:, 0] <= x_max) & (points[:, 0] >= x_min)]
 
     if graph:
         fig, ax = plt.subplots()
         ax.plot(0, 0, label='Turn Center')
-        ax.add_patch(Rectangle(r_center - track_width/2, 0), track_width, car_length)
-        x1 = np.linspace(r_center - track_width / 2, np.sqrt(np.pow(r_cf - track_width/2, 2) - np.pow(max_y, 2)), 100) # left boundary
-        x2 = np.linspace(r_center + track_width / 2, np.sqrt(np.pow(r_cf + track_width/2, 2) - np.pow(max_y, 2)), 100) # right boundary
-        y1 = np.sqrt(np.pow(r_center - track_width / 2, 2) - np.pow(x1, 2))
-        y2 = np.sqrt(np.pow(r_center + track_width / 2, 2) - np.pow(x2, 2))
+        ax.add_patch(Rectangle((r_center - track_width/2, 0),
+                     track_width, car_length))
+        x1 = np.linspace(r_center - track_width / 2, np.sqrt(np.power(r_cf -
+                         track_width/2, 2) - np.power(y_max, 2)), 100)  # left boundary
+        x2 = np.linspace(r_center + track_width / 2, np.sqrt(np.power(r_cf +
+                         track_width/2, 2) - np.power(y_max, 2)), 100)  # right boundary
+        y1 = np.sqrt(np.power(r_cf - track_width / 2, 2) - np.power(x1, 2))
+        y2 = np.sqrt(np.power(r_cf + track_width / 2, 2) - np.power(x2, 2))
         ax.plot(x1, y1)
         ax.plot(x2, y2)
         plt.show()
+        plt.savefig('plt.png')
 
     # check for points inside predicted car path
     return np.any(points[
-        points[:, 1] >= np.sqrt(
-            np.pow(points[:, 0], 2) + np.pow(r_cf - track_width/2, 2))
-        and
-        points[:, 1] <= np.sqrt(
-            np.pow(points[:, 0], 2) + np.pow(r_cf + track_width/2, 2))
-        and
-        points[:, 1] >= car_length
-        and
-        points[:, 1] <= y_max
+        (points[:, 1] >= np.sqrt(
+            np.power(points[:, 0], 2) + np.power(r_cf - track_width/2, 2)))
+        &
+        (points[:, 1] <= np.sqrt(
+            np.power(points[:, 0], 2) + np.power(r_cf + track_width/2, 2)))
+        &
+        (points[:, 1] >= car_length)
+        &
+        (points[:, 1] <= y_max)
     ])
 
 
-in_path()
+print(in_path(np.asarray([[0, 0], [1, 1]], dtype=np.float64), 4, 45))
