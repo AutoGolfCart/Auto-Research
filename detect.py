bf491486e0cf1aec55aff472635cc43a67b414d1
Benjamin Chauhan <63178260+Meeeee6623@users.noreply.github.com>
2022-05-20 20:41:45 -0400

    Fix detection bugs, right turns - fully functional

--- detect.py
+++ detect.py
@@ -1,7 +1,6 @@
 from hokuyolx import HokuyoLX
 import numpy as np
 
-
 track_width = 2.0  # wheel center to center distance of car
 forward_constant = 1.0  # multiplier for speed of car, adjust for proper braking distance
 car_length = 6.0  # length of car from front to back wheels, center to center
@@ -11,11 +10,11 @@ graph = True
 if graph:
     import matplotlib.pyplot as plt
     from matplotlib.patches import Rectangle
+    from matplotlib.patches import Arc
 
 
 def in_path(points, speed, angle):
-
-    r_center = car_length * np.sin(90 - angle)
+    r_center = car_length * np.tan(np.radians(90 - angle))  # left turn = positive angle
 
     # transform points to match new origin at turn center
     points[:, 0] += r_center
@@ -23,46 +22,104 @@ def in_path(points, speed, angle):
 
     r_cf = np.hypot(r_center, car_length)  # front center radius
 
-    y_max = forward_constant * speed 
+    r_left = np.hypot(r_center - track_width / 2, car_length)  # left wheel turn radius
+    r_right = np.hypot(r_center + track_width / 2, car_length)  # right wheel turn radius
 
-    x_min = r_center - track_width / \
-        2 if angle < 0 else np.sqrt(
-            np.power(r_cf - track_width/2, 2) - np.power(y_max, 2))
-    x_max = r_center + track_width / \
-        2 if angle > 0 else np.sqrt(
-            np.power(r_cf + track_width/2, 2) - np.power(y_max, 2))
+    y_max = car_length + forward_constant * speed
+
+    # check if y_max is past the turning circle
+    y_large = np.minimum(np.fabs(r_left), np.fabs(r_right)) < y_max
+    if y_large:
+        x_min = r_center - track_width / 2 if angle < 0 else 0
+        x_max = r_center + track_width / 2 if angle > 0 else 0
+    else:
+        x_min = r_center - track_width / 2 if angle < 0 else \
+            np.sqrt(
+                np.power(r_left, 2) - np.power(y_max, 2)
+            )
+        x_max = r_center + track_width / 2 if angle > 0 else \
+            -np.sqrt(
+                np.power(r_right, 2) - np.power(y_max, 2)
+            )
+
+    if graph:
+        fig, ax = plt.subplots()
+        # ax.plot(0, 0, label='Turn Center')
+        ax.add_patch(Rectangle((r_center - track_width / 2, 0),
+                               track_width, car_length))
+        # check if y_max is past the turning circle
+        if y_large:
+            x1 = np.linspace(r_center - track_width / 2, 0, 100)  # left boundary
+            x2 = np.linspace(r_center + track_width / 2, 0, 100)  # right boundary
+        else:
+            if angle < 0:
+                x1 = np.linspace(r_center - track_width / 2,
+                                 -np.sqrt(
+                                     np.power(r_left, 2) - np.power(y_max, 2)
+                                 ),
+                                 100)  # left boundary
+                x2 = np.linspace(r_center + track_width / 2,
+                                 -np.sqrt(
+                                     np.power(r_right, 2) - np.power(y_max, 2)
+                                 ),
+                                 100)  # right boundary
+            else:
+                x1 = np.linspace(r_center - track_width / 2,
+                                 np.sqrt(
+                                     np.power(r_left, 2) - np.power(y_max, 2)
+                                 ),
+                                 100)  # left boundary
+                x2 = np.linspace(r_center + track_width / 2,
+                                 np.sqrt(
+                                     np.power(r_right, 2) - np.power(y_max, 2)
+                                 ),
+                                 100)  # right boundary
+        y1 = np.sqrt(np.power(r_left, 2) - np.power(x1, 2))
+        y2 = np.sqrt(np.power(r_right, 2) - np.power(x2, 2))
+        ax.plot(x1, y1)
+        ax.plot(x2, y2)
+        if not y_large:
+            if angle < 0:
+                ax.plot(
+                    (-np.sqrt(np.power(r_left, 2) - np.power(y_max, 2)), -np.sqrt(np.power(r_right, 2) - np.power(y_max, 2))),
+                    (y_max, y_max))
+            else:
+                ax.plot(
+                    (x_min, -np.sqrt(np.power(r_right, 2) - np.power(y_max, 2))),
+                    (y_max, y_max))
+        # ax.plot((0, r_center + track_width / 2), (0, 0))
+        # ax.plot((0, r_center - track_width / 2), (0, car_length))
+        # ax.plot((0, r_center + track_width / 2), (0, car_length))
+        # ax.plot((0, r_center), (0, car_length))
+        # ax.add_patch(Arc((0, 0), width=r_cf * 2, height=r_cf * 2, theta1=0.0, theta2=90.0))
+        ax.scatter(points[:, 0], points[:, 1])
+        plt.show()
+        # plt.savefig('plt.png')
 
     # filter points to x range
     points = points[(points[:, 0] <= x_max) & (points[:, 0] >= x_min)]
 
-    if graph:
-        fig, ax = plt.subplots()
-        ax.plot(0, 0, label='Turn Center')
-        ax.add_patch(Rectangle((r_center - track_width/2, 0),
-                     track_width, car_length))
-        x1 = np.linspace(r_center - track_width / 2, np.sqrt(np.power(r_cf -
-                         track_width/2, 2) - np.power(y_max, 2)), 100)  # left boundary
-        x2 = np.linspace(r_center + track_width / 2, np.sqrt(np.power(r_cf +
-                         track_width/2, 2) - np.power(y_max, 2)), 100)  # right boundary
-        y1 = np.sqrt(np.power(r_cf - track_width / 2, 2) - np.power(x1, 2))
-        y2 = np.sqrt(np.power(r_cf + track_width / 2, 2) - np.power(x2, 2))
-        ax.plot(x1, y1)
-        ax.plot(x2, y2)
-        plt.show()
-        plt.savefig('plt.png')
-
-    # check for points inside predicted car path
-    return np.any(points[
-        (points[:, 1] >= np.sqrt(
-            np.power(points[:, 0], 2) + np.power(r_cf - track_width/2, 2)))
-        &
-        (points[:, 1] <= np.sqrt(
-            np.power(points[:, 0], 2) + np.power(r_cf + track_width/2, 2)))
-        &
-        (points[:, 1] >= car_length)
-        &
-        (points[:, 1] <= y_max)
-    ])
+    # check points
+    if angle < 0:
+        return np.any(points[
+                          (np.hypot(points[:, 0], points[:, 1]) > r_right)
+                          &
+                          (np.hypot(points[:, 0], points[:, 1]) < r_left)
+                          &
+                          (points[:, 1] > car_length)
+                          &
+                          (points[:, 1] < y_max)
+                          ])
+    else:
+        return np.any(points[
+                          (np.hypot(points[:, 0], points[:, 1]) < r_right)
+                          &
+                          (np.hypot(points[:, 0], points[:, 1]) > r_left)
+                          &
+                          (points[:, 1] > car_length)
+                          &
+                          (points[:, 1] < y_max)
+                          ])
 
 
-print(in_path(np.asarray([[0, 0], [1, 1]], dtype=np.float64), 4, 45))
+print(in_path(np.asarray([[0, .5], [1, 1]], dtype=np.float64), 5, -30))
